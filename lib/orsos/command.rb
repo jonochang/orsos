210d51f553d84426ef4ca2f247e3738055072f20
Jonathan Chang <jonathan.chang@silverpond.com.au>
2014-10-21 22:51:06 -0700

    fix require_relative

--- lib/orsos/command.rb
+++ lib/orsos/command.rb
@@ -1,5 +1,5 @@
 require 'thor'
-require_relative './commands/get_command'
+require_relative './commands/get'
 class Orsos::Command < Thor
   register Orsos::Commands::Get, 'get', 'get [COMMAND]', 'downloads excel files from website'
   #register Orsos::ConvertCommand, 'convert', 'convert [COMMAND]', 'downloads and converts xls to csv'
